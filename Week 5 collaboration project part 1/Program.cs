2041cbd2fb98bc8d49c270f2c034660f5664001b
Joeyrmuzzo <57329326+Joeyrmuzzo@users.noreply.github.com>
2020-03-31 14:32:49 -0400

    Add files via upload

---
+++ Week 5 collaboration project part 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_5_collaboration_project_part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+          int  number = 4;
+          int  guesscount = 0;
+          int guess = 0;
+            Console.WriteLine("Guess a number between 1 and 10: ");
+            guess = Int32.Parse(Console.ReadLine());
+            guesscount = guesscount + 1;
+            while (guess != number) 
+            {
+
+                if (guess < number)
+
+                    Console.WriteLine("Your guess is too low");
+
+                else if (guess > number)
+
+                    Console.WriteLine("Your guess is too high");
+
+                else
+                {
+                    Console.WriteLine("You got it!!");
+                    
+                }
+                Console.WriteLine("Guess again: ");
+                guess = Int32.Parse(Console.ReadLine());
+                guesscount = guesscount + 1;
+                //ask user for input again
+
+            }
+
+
+            Console.WriteLine("You figured it out in {0} guesses. ", guesscount);
+            Console.ReadLine();
+
+
+        }
+    }
+}
